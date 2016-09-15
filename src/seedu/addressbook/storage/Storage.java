49b19ee6a4d569bea087a36b4623000f58d6c0d5
GitMeGet <tanyupeng1994@gmail.com>
2016-09-15 20:02:39 +0800

    Apply Dependency Inversion Principle [LO-DIP]

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public interface Storage {
+
+    abstract public String getPath();
+    abstract public AddressBook load() throws StorageOperationException;
+    abstract public void save(AddressBook addressBook) throws StorageOperationException;
+}
