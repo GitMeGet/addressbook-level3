27bb94bc61c69b21b596d3c83291b0a8b163a282
GitMeGet <tanyupeng1994@gmail.com>
2016-09-16 22:17:03 +0800

    Use Dependency Injection [LO-DI]

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,26 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public class StorageStub implements Storage {
+
+    
+    @Override
+    public String getPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public AddressBook load() throws StorageOperationException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public void save(AddressBook addressBook) throws StorageOperationException {
+        // TODO Auto-generated method stub
+        
+    }
+}
